d452dd0b4cd6fe45b800cb547b9458d2af7c65af
user <user@LAPTOP-VCVPT2J9>
2023-04-20 13:37:01 +0300

    Добавьте файлы проекта.

---
+++ Names/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Names
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
+                lstNames.Items.Add(txtName.Text);
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= 48 && e.KeyChar <= 57)
+            {
+                MessageBox.Show($"Form.KeyPress: '{e.KeyChar}' pressed.");
+
+                switch (e.KeyChar)
+                {
+                    case (char)49:
+                    case (char)52:
+                    case (char)55:
+                        MessageBox.Show($"Form.KeyPress: '{e.KeyChar} consumed.");
+                        e.Handled = true;
+                        break;
+                }
+            }    
+        }
+
+        private void btnAdd_MouseEnter(object sender, EventArgs e)
+        {
+            btnAdd.Cursor = System.Windows.Forms.Cursors.Hand;
+        }
+    }
+}
